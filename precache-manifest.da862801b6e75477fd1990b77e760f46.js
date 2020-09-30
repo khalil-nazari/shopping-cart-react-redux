28999f80f59e9292acc7c36ec6c437486662d780
khalil Ahmad <khalil.nazari4@gmail.com>
2020-09-30 21:43:45 +0800

    Updates

---
+++ precache-manifest.da862801b6e75477fd1990b77e760f46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e3ee72f096c0ff1b6b72055e7750227",
+    "url": "/shopping-cart-react-redux/index.html"
+  },
+  {
+    "revision": "cbefb06c9fa020e4a459",
+    "url": "/shopping-cart-react-redux/static/css/main.6f20e00e.chunk.css"
+  },
+  {
+    "revision": "44a5a2b8902ce4f7c6f2",
+    "url": "/shopping-cart-react-redux/static/js/2.b16ae2d3.chunk.js"
+  },
+  {
+    "revision": "4845593b3544c4878bbf7b7c87ce3e13",
+    "url": "/shopping-cart-react-redux/static/js/2.b16ae2d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbefb06c9fa020e4a459",
+    "url": "/shopping-cart-react-redux/static/js/main.9bcd7d4e.chunk.js"
+  },
+  {
+    "revision": "f64ee6853256d5a8350a",
+    "url": "/shopping-cart-react-redux/static/js/runtime-main.b0c69a2b.js"
+  }
+]);
